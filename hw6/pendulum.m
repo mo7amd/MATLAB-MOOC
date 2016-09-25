3831878029e39333d28c21ee2d00bac61132ef85
Mohamed.k.Khalil <mo7amd.khaled@gmail.com>
2016-09-25 09:15:07 +0200

    up date solution of problem 8

--- hw6/pendulum.m
+++ hw6/pendulum.m
@@ -1,17 +1,18 @@
-function [T] = pendulum(L,angle0)
+function [T] = pendulum(L,a0)
+%    a0 postive integer less than pi 
     T = 0;
     if L > 0
-        dt = 1e-6;
+        dt = 1.e-6;
+        a_velocity = 0; 
         g = 9.8;
-        angle = abs(angle0);
-        omega = 0;
-        T = 0;
-        while angle > 0
-           a = g*sin(angle)/L;
-           omega = omega + dt * a;
-           angle = angle - dt * omega;
-           T = T + dt;
+        theta = abs(a0); 
+        while theta > 0
+            a_acceleration = g*sin(theta)/L;
+            a_velocity = a_velocity + dt * a_acceleration;
+            theta = theta - dt * a_velocity;
+            T = T + dt;
         end
-        T = T * 4;
+    T = T*4;
     end
 end
+
